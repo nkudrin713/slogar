7bb273f0ee1ea629cb94c39bd80730f7572e1392
nkudrin713 <nik123.93@yandex.ru>
2026-06-20 15:17:18 +0300

    Hide internal syllabifier options

--- src/main/kotlin/dev/nkudrin/slogar/RussianSyllabifierOptions.kt
+++ src/main/kotlin/dev/nkudrin/slogar/RussianSyllabifierOptions.kt
@@ -7,7 +7,7 @@ package dev.nkudrin.slogar
  * @property normalizeYo whether `ё` should be treated as `е` during analysis; output is unchanged
  * @property strict whether invalid input and unresolved boundaries should throw an exception
  */
-data class RussianSyllabifierOptions(
+internal data class RussianSyllabifierOptions(
     val policy: SyllablePolicy = SyllablePolicy.SchoolSimple,
     val normalizeYo: Boolean = false,
     val strict: Boolean = false,
