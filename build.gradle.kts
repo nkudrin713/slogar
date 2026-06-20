a3155861d0361107cad1b3ccf5a996add23ddb3b
nkudrin713 <nik123.93@yandex.ru>
2026-06-20 18:32:05 +0300

    Configure Central Portal publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,14 @@ import org.gradle.jvm.tasks.Jar
 
 plugins {
     `java-library`
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.36.0"
     kotlin("jvm") version "2.4.0"
 }
 
 group = "dev.nkudrin"
-version = "0.1.0-SNAPSHOT"
+version = providers.gradleProperty("releaseVersion")
+    .orElse("0.1.0-SNAPSHOT")
+    .get()
 
 kotlin {
     jvmToolchain(17)
@@ -32,40 +34,37 @@ tasks.withType<Jar>().configureEach {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 
-            artifactId = "slogar"
+    coordinates(group.toString(), "slogar", version.toString())
 
-            pom {
-                name = "slogar"
-                description = "Kotlin library for syllabification of Russian words"
-                url = "https://github.com/nkudrin713/slogar"
+    pom {
+        name = "slogar"
+        description = "Kotlin library for syllabification of Russian words"
+        url = "https://github.com/nkudrin713/slogar"
 
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                        distribution = "repo"
-                    }
-                }
-
-                developers {
-                    developer {
-                        id = "nkudrin713"
-                        name = "Nikita Kudrin"
-                        url = "https://github.com/nkudrin713"
-                    }
-                }
-
-                scm {
-                    connection = "scm:git:https://github.com/nkudrin713/slogar.git"
-                    developerConnection = "scm:git:ssh://git@github.com/nkudrin713/slogar.git"
-                    url = "https://github.com/nkudrin713/slogar"
-                }
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "repo"
             }
+        }
+
+        developers {
+            developer {
+                id = "nkudrin713"
+                name = "Nikita Kudrin"
+                url = "https://github.com/nkudrin713"
+            }
+        }
+
+        scm {
+            connection = "scm:git:https://github.com/nkudrin713/slogar.git"
+            developerConnection = "scm:git:ssh://git@github.com/nkudrin713/slogar.git"
+            url = "https://github.com/nkudrin713/slogar"
         }
     }
 }
